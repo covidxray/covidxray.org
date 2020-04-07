e5fcc350ef1fd82f67e1b1769c72d9f35acac28a
Thaung Monyodam <you@example.com>
2020-04-07 23:05:48 +0700

    fefeat: dashboard

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+
 <h1 align="center">
   <br>
   <br>
@@ -14,22 +16,38 @@
 ## Backend
    - FLASK
 
-## Build Setup
+## Build from sources
 
-``` bash
-cd viewmyai-covid-xray
-# run virtual environments
-python3 -m venv  v
-# activate
-source env/bin/activate
-# install dependencies
-pip3 install -r requirments.txt
-# serve with hot reload at localhost:5000
-python3 app.py 
-
-
+```bash
+$ # Clone the sources
+$ git clone https://github.com/covidxray/covidxray.org.git
+$ cd covidxray.org
+$
+$ # Virtualenv modules installation (Unix based systems)
+$ virtualenv --no-site-packages covid-19
+$ source covid-19/bin/activate
+$
+$ # Virtualenv modules installation (Windows based systems)
+$ # virtualenv --no-site-packages env
+$ # .\env\Scripts\activate
+$ 
+$ # Install requirements
+$ pip3 install -r requirements.txt
+$
+$ # Set the FLASK_APP environment variable
+$ (Unix/Mac) export FLASK_APP=run.py
+$ (Windows) set FLASK_APP=run.py
+$ (Powershell) $env:FLASK_APP = ".\run.py"
+$
+$ # Set up the DEBUG environment
+$ # (Unix/Mac) export FLASK_ENV=development
+$ # (Windows) set FLASK_ENV=development
+$ # (Powershell) $env:FLASK_ENV = "development"
+$
+$ # Run the application
+$ # --host=0.0.0.0 - expose the app on all network interfaces (default 127.0.0.1)
+$ # --port=5000    - specify the app port (default 5000)  
+$ flask run --host=0.0.0.0 --port=5000
+$
+$ # Access the app in browser: http://127.0.0.1:5000/
 ```
-
-## Setup Virtual environments
-For detailed explanation on how things work, checkout the [guide](https://docs.python.org/3/library/venv.html)
-
---
+++ app/views.py
@@ -0,0 +1,206 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Python modules
+import os, logging 
+
+# Flask modules
+from flask               import render_template, request, url_for, redirect, jsonify,send_from_directory
+from flask_login         import login_user, logout_user, current_user, login_required
+from werkzeug.exceptions import HTTPException, NotFound, abort
+
+# App modules
+from app        import app, lm, db, bc
+from app.models import User
+from app.forms  import LoginForm, RegisterForm
+
+import os
+import uuid
+from .chexnet.chexnet import Xray
+from .util import base64_to_pil, np_to_base64, base64_to_bytes
+import numpy as np
+import torch
+
+x_ray = Xray()
+
+# provide login manager with load_user callback
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Logout user
+@app.route('/logout.html')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Register a new user
+@app.route('/register.html', methods=['GET', 'POST'])
+def register():
+    
+    # declare the Registration Form
+    form = RegisterForm(request.form)
+
+    msg = None
+
+    if request.method == 'GET': 
+
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/register.html', form=form, msg=msg ) )
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+        email    = request.form.get('email'   , '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        # filter User out of database through username
+        user_by_email = User.query.filter_by(email=email).first()
+
+        if user or user_by_email:
+            msg = 'Error: User exists!'
+        
+        else:         
+
+            pw_hash = password #bc.generate_password_hash(password)
+
+            user = User(username, email, pw_hash)
+
+            user.save()
+
+            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
+
+    else:
+        msg = 'Input error'     
+
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/register.html', form=form, msg=msg ) )
+
+# Authenticate user
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    
+    # Declare the login form
+    form = LoginForm(request.form)
+
+    # Flask message injected into the page, in case of any errors
+    msg = None
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        if user:
+            
+            #if bc.check_password_hash(user.password, password):
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('dashboard'))
+            else:
+                msg = "Wrong password. Please try again."
+        else:
+            msg = "Unkkown user"
+
+    return render_template('layouts/auth-default.html',
+                            content=render_template( 'pages/login.html', form=form, msg=msg ) )
+
+@app.route('/index.html', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('layouts/default.html',
+                            content=render_template( 'pages/index.html') )
+
+# App main route + generic routing
+@app.route('/', defaults={'path': 'home.html'})
+@app.route('/<path>')
+def index(path):
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+    
+    if path == "home.html":
+        return render_template('layouts/home-default.html',
+                                content=render_template( 'pages/'+path) )
+
+    content = None
+
+    try:
+
+        # try to match the pages defined in -> pages/<input file>
+        return render_template('layouts/default.html',
+                                content=render_template( 'pages/'+path) )
+    except:
+        
+        return render_template('layouts/auth-default.html',
+                                content=render_template( 'pages/404.html' ) )
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        img = base64_to_pil(request.json)
+    except Exception:
+        return jsonify(error="expected string or bytes-like object"), 422
+
+    rgb_image_np = np.array(img.convert('RGB'))
+    stds = rgb_image_np.std(axis=(0, 1))
+    means = rgb_image_np.mean(axis=(0, 1))
+    if 35 < np.average(stds) < 95 and 60 < np.average(means) < 180:
+        img_result = x_ray.predict(img)
+        condition_similarity_rate = []
+        if img_result['condition rate'] == []:
+            return jsonify(
+                result='NOT DETECTED',
+                error='Invalid image'
+            ), 200
+        else:
+            similarity_rate = 0.0001
+            types = ""
+            case = "HEALTY"
+            for name, prob in img_result['condition rate']:
+                if round(float(prob), 3) > similarity_rate:
+                   similarity_rate = round(float(prob), 3)
+                   types = name
+                   if name == "COVID-19" or name == "Pneumonia":
+                      case = "NOT HEALTY" 
+                   else:
+                      case = "HEALTY"
+            return jsonify(
+               prob = str(round(float(similarity_rate*100), 3))+"%",
+               types = str(types),
+               case = case
+            ), 200
+    else:
+        file_name = "NOT_DETECTED/%s.jpg" % str(uuid.uuid4())
+        return jsonify(
+            result='NOT DETECTED',
+            error='Invalid image'
+        ), 200
+
+
+# Return sitemap 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
+
+@app.route('/offline.html')
+def offline():
+    return app.send_static_file('offline.html')
+
+
+@app.route('/service-worker.js')
+def sw():
+    return app.send_static_file('service-worker.js')
