a01f871f4c01dd5a707e7b075e517490382d7995
Thaung Monyodam <you@example.com>
2020-04-08 08:08:30 +0700

    https config

--- run.py
+++ run.py
@@ -6,14 +6,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 from app import app, db
 
-class ReverseProxied(object):
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        return self.app(environ, start_response)
-
-app.wsgi_app = ReverseProxied(app.wsgi_app)
+app.config.update(dict(
+  PREFERRED_URL_SCHEME = 'https'
+))
